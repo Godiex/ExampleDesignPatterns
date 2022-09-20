535e06eddcc3eadf35cc4d64cd7af44934633b4c
Diego Villa <diego.villa@devzeros.com>
2022-09-20 09:17:18 -0500

    patron builder finalizado

---
+++ Builder/Domain.Tests/CellVehicleTest/CellVehicleDataBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Domain.Tests.CellVehicleTest
+{
+    public class CellVehicleDataBuilder
+    {
+        private Vehicle _vehicle = default!;
+        private Cell _cell = default!;
+        private DateTime _entryDate;
+        private DateTime _departureDate;
+
+        public Domain.Entities.CellVehicle Build()
+        {
+            return new Entities.CellVehicle
+            {
+                Vehicle = _vehicle,
+                Cell = _cell,
+                DepartureDate = _departureDate,
+                EntryDate = _entryDate
+            };
+        }
+
+        public CellVehicleDataBuilder WithCell(Cell cell)
+        {
+            _cell = cell;
+            return this;
+        }
+
+        public CellVehicleDataBuilder WithVehicle(Vehicle vehicle)
+        {
+            _vehicle = vehicle;
+            return this;
+        }
+
+        public CellVehicleDataBuilder WithEntryDate(DateTime entryDate)
+        {
+            _entryDate = entryDate;
+            return this;
+        }
+
+        public CellVehicleDataBuilder WithDepartureDate(DateTime departureDate)
+        {
+            _departureDate = departureDate;
+            return this;
+        }
+
+    }
+}
---
+++ Builder/Domain.Tests/ParkingInvoice/ParkingInvoiceTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Tests.ParkingInvoice
+{
+    [TestClass]
+    public class ParkingInvoiceTest
+    {
+        private Entities.ParkingValues _parkingValues = default!;
+
+        [TestInitialize]
+        public void Init() 
+        {
+            _parkingValues = new Entities.ParkingValues
+            {
+                MotorcycleHourValue = 500,
+                DayMotorcycleValue = 4000,
+                CarHourValue = 1000,
+                CarDayValue = 8000,
+                ValueSurchange = 2000
+            };
+        }
+
+        [TestMethod]
+        public void CalculatingTotalValueWhenNumberHoursIsMinorToNine() 
+        {
+            var invoiceParking = new ParkingInvoiceDataBuilder()
+                .WithCellVehicle( new Entities.CellVehicle { 
+                    Vehicle = new Entities.Vehicle { Type = "Carro" },
+                    EntryDate = DateTime.Now.AddHours(-8),
+                    DepartureDate = DateTime.Now
+                })
+                .WithParkingValues(_parkingValues)
+                .Build();
+
+            invoiceParking.CalculateTotalValue();
+
+            Assert.AreEqual(8000, invoiceParking.TotalValue);
+        }
+
+        [TestMethod]
+        public void CalculatingTotalValueWhenVehicleIsMotorcycleAndTheCylinderIsGreaterThanFiveHundred()
+        {
+            var invoiceParking = new ParkingInvoiceDataBuilder()
+                .WithCellVehicle(new Entities.CellVehicle
+                {
+                    Vehicle = new Entities.Vehicle { Type = "Motocicleta", Cylinders = 650 },
+                    EntryDate = DateTime.Now.AddHours(-10),
+                    DepartureDate = DateTime.Now
+                })
+                .WithParkingValues(_parkingValues)
+                .Build();
+
+            invoiceParking.CalculateTotalValue();
+
+            Assert.AreEqual(6000, invoiceParking.TotalValue);
+        }
+
+        [TestMethod]
+        public void CalculatingTotalValueWhenVehicleIsCarAndTheTimeInParkingIsGreaterThanOneDay()
+        {
+            var invoiceParking = new ParkingInvoiceDataBuilder()
+                .WithCellVehicle(new Entities.CellVehicle
+                {
+                    Vehicle = new Entities.Vehicle { Type = "Car"},
+                    EntryDate = DateTime.Now.AddHours(-27),
+                    DepartureDate = DateTime.Now
+                })
+                .WithParkingValues(_parkingValues)
+                .Build();
+
+            invoiceParking.CalculateTotalValue();
+
+            Assert.AreEqual(11000, invoiceParking.TotalValue);
+        }
+
+        [TestMethod]
+        public void CalculatingTotalValueWhenVehicleIsMotorcycleAndTheTimeInParkingIsGreaterThanOneDay()
+        {
+            var invoiceParking = new ParkingInvoiceDataBuilder()
+                .WithCellVehicle(new Entities.CellVehicle
+                {
+                    Vehicle = new Entities.Vehicle { Type = "Motocicleta", Cylinders = 125 },
+                    EntryDate = DateTime.Now.AddHours(-51),
+                    DepartureDate = DateTime.Now
+                })
+                .WithParkingValues(_parkingValues)
+                .Build();
+
+            invoiceParking.CalculateTotalValue();
+
+            Assert.AreEqual(9500, invoiceParking.TotalValue);
+        }
+
+    }
+}
